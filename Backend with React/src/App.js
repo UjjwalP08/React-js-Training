f196670b63b35f74e9a12645bb0fb8750a622dbe
Ujjwal Patel <UjjWALP476@gmail.com>
2023-03-16 13:07:46 +0530

    Error handling and Basic Loading for http req in React

--- Backend with React/src/App.js
+++ Backend with React/src/App.js
@@ -1,28 +1,59 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  function fetchMovieDataHandler() {
-    fetch("https://swapi.dev/api/films")
-      .then((Response) => {
-        return Response.json();
-      })
-      .then((data) => {
-        const moviesInfo = data.results.map(movieData => {
-          return{
-            id:movieData.episode_id,
-            title:movieData.title,
-            openingText:movieData.opening_crawl,
-            releaseDate:movieData.release_date
-          };
-        })
-        setData(moviesInfo);
+  async function fetchMovieDataHandler() {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+
+      if (!response.ok) {
+        throw new Error("Something Went Wrong.....");
+      }
+
+      const data = await response.json();
+
+      const moviesInfo = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
       });
+      setData(moviesInfo);
+    } catch (error) {
+      // error message
+      setError(error.message);
+    }
+
+    setIsLoading(false);
   }
+
+  let content = <p>No Movies Found...</p>;
+
+  if(data.length > 0)
+  {
+    content = <MoviesList movies={data} />
+  }
+
+  if(error)
+  {
+    content = <p>Something Went Wrong...</p>;
+  }
+
+  if(isLoading)
+  {
+    content = <p>Loading.....</p>;
+  }
+
 
   return (
     <React.Fragment>
@@ -30,7 +61,7 @@ function App() {
         <button onClick={fetchMovieDataHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={data} />
+        {content}
       </section>
     </React.Fragment>
   );
