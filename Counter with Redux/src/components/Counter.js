d44136ac76ab214edc6f0760ffa9f086fc07d241
Ujjwal Patel <UjjWALP476@gmail.com>
2023-03-20 16:15:57 +0530

    useDispatch() hook to dispatch action in Counter.js

--- Counter with Redux/src/components/Counter.js
+++ Counter with Redux/src/components/Counter.js
@@ -1,9 +1,16 @@
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
+  const incrementHandler = ()=>{
+    dispatch({type:"increment"})
+  }
+  const decrementHandler = ()=>{
+    dispatch({type:"decrement"})
+  }
   const toggleCounterHandler = () => {};
 
 
@@ -11,6 +18,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{count}</div>
+      <div>
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
