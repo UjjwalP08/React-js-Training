95312b0fb5282c4f6d32bffcb98af943d1edb54b
Ujjwal Patel <UjjWALP476@gmail.com>
2023-03-20 15:17:59 +0530

    Inital Commit

---
+++ Redux DEMO/redux-demo.js
@@ -0,0 +1,35 @@
+// import {  createStore } from 'redux';
+
+const redux = require('redux');
+
+const counterReducer = (state = {counter:0},action)=>{
+
+    if(action.type === 'INC')
+    {
+        return{
+            counter:state.counter + 1
+        }   
+    }
+    if(action.type === 'DEC')
+    {
+        return{
+            counter:state.counter - 1
+        }
+    }
+
+    
+
+}
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = ()=>{
+    const latestState = store.getState();
+    console.log(latestState);
+}
+
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({type:'INC'});
+store.dispatch({type:'DEC'});
