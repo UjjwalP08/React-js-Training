0bef629236a60a7c6fd10ce5d6ca2dd4817ad02e
Ujjwal Patel <UjjWALP476@gmail.com>
2023-03-24 13:24:33 +0530

    Fetch the data from firebase

--- Food App/src/components/Meals/AvailableMeal.js
+++ Food App/src/components/Meals/AvailableMeal.js
@@ -1,38 +1,41 @@
+import { useState, useEffect } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeal.module.css";
 import MealItems from "./MealItems/MealItems";
 
-// in this file we have dummy data that we render in our Webpage
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 220.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 260.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 130.09,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 180.99,
-  },
-];
+// we not use dummy data any more now we fetch the data from firebase
 
 const AvailableMeal = () => {
   // we can Wrap the meallist using MealItem componet where we pass our props
-  const mealList = DUMMY_MEALS.map((meal) => {
+
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    // we not use async & await in useEffect main function to use async and await need to make new function inside that function
+
+    const fetchData = async () => {
+      const data = await fetch(
+        "https://food-app-be1c4-default-rtdb.firebaseio.com/meals.json"
+      );
+      const resoponse = await data.json();
+
+      const loadMeals = [];
+
+      for (const key in resoponse)
+       {
+        loadMeals.push({
+          id: key,
+          name: resoponse[key].name,
+          description: resoponse[key].description,
+          price: resoponse[key].price,
+        });
+      }
+      setMeals(loadMeals)
+    };
+
+    fetchData();
+  }, []);
+  const mealList = meals.map((meal) => {
     return (
       <MealItems
         key={meal.id}
