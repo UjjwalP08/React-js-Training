d3722ced9a065064fa85d6fc2eec59ca3aeb246a
Ujjwal Patel <UjjWALP476@gmail.com>
2023-03-24 14:46:36 +0530

    add error handling

--- Food App/src/components/Meals/AvailableMeal.js
+++ Food App/src/components/Meals/AvailableMeal.js
@@ -9,6 +9,8 @@ const AvailableMeal = () => {
   // we can Wrap the meallist using MealItem componet where we pass our props
 
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
 
   useEffect(() => {
     // we not use async & await in useEffect main function to use async and await need to make new function inside that function
@@ -17,12 +19,16 @@ const AvailableMeal = () => {
       const data = await fetch(
         "https://food-app-be1c4-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if(!data.ok)
+      {
+        throw new Error("Something went wrong!!!!");
+      }
       const resoponse = await data.json();
 
       const loadMeals = [];
 
-      for (const key in resoponse)
-       {
+      for (const key in resoponse) {
         loadMeals.push({
           id: key,
           name: resoponse[key].name,
@@ -30,11 +36,31 @@ const AvailableMeal = () => {
           price: resoponse[key].price,
         });
       }
-      setMeals(loadMeals)
+      setMeals(loadMeals);
+      setIsLoading(false);
     };
 
-    fetchData();
+    fetchData().catch((error)=>{
+      setIsLoading(false);
+      setError(error.message);
+    });
   }, []);
+
+
+
+  if (isLoading) {
+    return (
+      <section className={classes.loading}>
+        <p>Loading data....</p>
+      </section>
+    );
+  }
+
+  if(error){
+    return (<section className={classes.error}>
+      <p>{error}</p>
+    </section>)
+  }
   const mealList = meals.map((meal) => {
     return (
       <MealItems
