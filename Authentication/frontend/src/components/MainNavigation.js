77ee0cbfa72f99f06de5c7ad89c5ea0fc2bd9216
Ujjwal Patel <UjjWALP476@gmail.com>
2023-03-28 15:51:30 +0530

    Validate email through a backend and adding new data in backend

--- Authentication/frontend/src/components/MainNavigation.js
+++ Authentication/frontend/src/components/MainNavigation.js
@@ -41,7 +41,7 @@ function MainNavigation() {
           </li>
           <li>
             <NavLink
-              to="/auth?mode=Login"
+              to="/auth?mode=login"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
--- Authentication/frontend/src/pages/Authentication.js
+++ Authentication/frontend/src/pages/Authentication.js
@@ -1,7 +1,45 @@
 import AuthForm from '../components/AuthForm';
+import { json, redirect } from 'react-router-dom';
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({request}){
+  
+  const searchParmas = new URL(request.url).searchParams;
+  const mode = searchParmas.get('mode') || 'login';
+  
+  if(mode !== 'login' && mode!== 'signup')
+  {
+    throw json({message:'Invalid mode'},{status:422});
+  }
+  const data = await request.formData();
+
+    const authData = {
+      email:data.get('email'),
+      password:data.get('password')
+    };
+
+    const response = await fetch('http://localhost:8080/' + mode,{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(authData)
+    });
+
+    if(response.status === 422 || response.status===401)
+    {
+      return response;
+    }
+
+    if(!response.ok)
+    {
+      throw json({message:"Can't Authenticat User.."},{status:500})
+    }
+
+    return redirect('/');
+}
