60dcdff35b2f65c5f578334afecea527a2fea208
Ujjwal Patel <UjjWALP476@gmail.com>
2023-03-30 14:50:37 +0530

    make working showDetail button in homepage using useRouter() hook

---
+++ next_js_app/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+const MeetupIds = () => {
+  return (
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1679237526223-8a5cbe0ef7a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM5fE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+      title="Building"
+      address="Baker Street,London"
+      descripton="night image of building in london"
+    />
+  );
+};
+
+export default MeetupIds;
--- next_js_app/pages/index.js
+++ next_js_app/pages/index.js
@@ -1,23 +1,34 @@
 import MeetupList from "@/components/meetups/MeetupList";
+import { useEffect, useState } from "react";
 
 const DUMMY_DATA = [
-    {id:"m1",
-    image:'https://images.unsplash.com/photo-1679237526223-8a5cbe0ef7a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM5fE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60' ,
-    title:"Building",
-    address:"Baker Street,London",
-    description:"night image of building in london"
-},
-    {id:"m2",
-    image:'https://images.unsplash.com/photo-1677616301815-9f446498da3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' ,
-    title:"Buildings",
-    address:"Torrento,Canda",
-    description:"Sunset time image of buildings"
-},
-]
+  {
+    id: "m1",
+    image:
+      "https://images.unsplash.com/photo-1677616301815-9f446498da3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    title: "Buildings",
+    address: "Torrento,Canda",
+    description: "Sunset time image of buildings",
+  },
+  {
+    id: "m2",
+    image:
+      "https://images.unsplash.com/photo-1679237526223-8a5cbe0ef7a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM5fE04alZiTGJUUndzfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+    title: "Building",
+    address: "Baker Street,London",
+    description: "night image of building in london",
+  },
+];
 
-const Homepage = ()=>{
-    return <MeetupList meetups={DUMMY_DATA} />
+const Homepage = () => {
+  const [loadMeetups, setLoadMeetups] = useState([]);
+
+  useEffect(() => {
+    // send http requests and fetch data
+    setLoadMeetups(DUMMY_DATA);
+  }, []);
+
+  return <MeetupList meetups={loadMeetups} />;
 };
 
 export default Homepage;
-
