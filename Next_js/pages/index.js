3f3b6df781c687dfa680846f38bbd5a051b63ebd
Ujjwal Patel <UjjWALP476@gmail.com>
2023-03-30 11:58:48 +0530

    Add page dynamically and Naviagating pages

--- Next_js/pages/index.js
+++ Next_js/pages/index.js
@@ -1,6 +1,23 @@
-function Homepage()
-{
-    return <h1>The Home page</h1>
-};
+import Link from "next/link";
+
+function Homepage() {
+    
+  return (
+    <>
+      <h1>The Home page</h1>
+      <br />
+
+      <ul>
+        <li>
+            <button>
+            <Link href='/news'>
+                Go to News page
+            </Link>
+            </button>
+        </li>
+      </ul>
+    </>
+  );
+}
 
 export default Homepage;
--- Next_js/pages/news.js
+++
@@ -1,6 +0,0 @@
-function News()
-{
-    return <h1>The News page</h1>
-};
-
-export default News;
