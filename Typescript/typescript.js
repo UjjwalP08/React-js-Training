4bf611eb5aebecaa0866ed41f262b1ed3ff7a08f
Ujjwal Patel <UjjWALP476@gmail.com>
2023-03-31 16:57:25 +0530

    Array and object in TS

--- Typescript/typescript.js
+++ Typescript/typescript.js
@@ -8,3 +8,17 @@ var age = 34;
 var msg = "Hello";
 var isValid = true;
 // console.log(age,msg,isValid);
+// Object and array in TS
+// Array
+var arr;
+arr = ["hello", "how are you", "good"];
+console.log(arr);
+// object
+var obj1;
+obj1 = {
+    name: "Ujjwal",
+    age: 21,
+};
+console.log((obj1));
+// Array of an object
+var peopl;
--- Typescript/typescript.ts
+++ Typescript/typescript.ts
@@ -4,17 +4,42 @@ function add(a: number, b: number) {
 
 const result = add(2, 5);
 
-console.log(result);
+// console.log(result);
 
 // primitive data types
 
-let age:number = 34;
+let age: number = 34;
 
-let msg:string="Hello";
+let msg: string = "Hello";
 
-let isValid:boolean=true;
+let isValid: boolean = true;
 
 // console.log(age,msg,isValid);
 
+// Object and array in TS
+
+// Array
+let arr: string[];
+arr = ["hello", "how are you", "good"];
+console.log(arr);
 
 
+// object
+let obj1: {
+  name: string;
+  age: number;
+};
+
+obj1 = {
+  name: "Ujjwal",
+  age: 21,
+};
+
+console.log((obj1));
+
+
+// Array of an object
+let peopl:{
+    name:string,
+    age:number
+}
