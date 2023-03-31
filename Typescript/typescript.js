91f8d1938deaae33aee78a6391deda35dbfa36e8
Ujjwal Patel <UjjWALP476@gmail.com>
2023-03-31 16:51:49 +0530

    Inital Commit

---
+++ Typescript/typescript.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+    return a + b;
+}
+var result = add(2, 5);
+console.log(result);
+// primitive data types
+var age = 34;
+var msg = "Hello";
+var isValid = true;
+// console.log(age,msg,isValid);
---
+++ Typescript/typescript.ts
@@ -0,0 +1,20 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+
+const result = add(2, 5);
+
+console.log(result);
+
+// primitive data types
+
+let age:number = 34;
+
+let msg:string="Hello";
+
+let isValid:boolean=true;
+
+// console.log(age,msg,isValid);
+
+
+
