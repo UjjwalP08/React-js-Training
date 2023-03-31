efc1b9c4908664aacb63594016127ab9b6e1e8b8
Ujjwal Patel <UjjWALP476@gmail.com>
2023-03-31 15:21:02 +0530

    Testing Async component proper way

--- Testing in React/src/components/Async.test.js
+++ Testing in React/src/components/Async.test.js
@@ -2,7 +2,12 @@ import { render,screen } from "@testing-library/react"
 import Async from "./Async"
 
 describe('Async Component',()=>{
-    test('render the Async Component',async ()=>{
+    test('render the Async Component', async ()=>{
+
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json:async () => [{id:'p1',title:'First Post'}],
+        })  ;
         render(<Async />)
 
         // listitem is indicate all the list which return the value
